b16484f25300e651788304d93ff84bcc52fc0e6e
EntityPENGIN <nopedoritos@gmail.com>
2023-05-23 14:14:25 +0900

    2

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from discord.ext import tasks
 from server import keep_alive
 
 discord_token = os.environ["DISCORD_TOKEN"]
+main_channel_id = int(os.environ["MAIN_CHANNEL_ID"])
 bot_channel_id = int(os.environ["BOT_CHANNEL_ID"])
 
 intents = discord.Intents.default()
@@ -24,22 +25,28 @@ class Pengan(discord.Client):
     async def on_ready(self):
         print(f"We have logged in as {self.user}")
         await self.change_presence(activity=discord.Game(name="!!help", type=1))
+        await client.get_channel(bot_channel_id).send(f"We have logged in as {self.user}")
 
-        # async for message in client.get_channel(1064131569159258183).history(limit=20):
+        # async for message in client.get_channel().history(limit=20):
         #     print(message.content)
-        # await client.get_channel(1060224631216287816).send("""フライングありなのそれ""")
-        # await client.get_channel(bot_channel_id).send("""熱帯夜あつい""")
+        # await client.get_channel().send("""フライングありなのそれ""")
 
         loop.start()
 
     async def on_message(self, message: discord.Message):
         print(f"""On {message.channel}, {message.channel.guild} ({message.channel.id})
-{message.author}: {message.content}""")
+{message.author}: {message.content}
+""")
         if message.author == self.user:
             return
         if message.content.startswith("!!help"):
             await message.channel.send("ヘルプ: !!help")
-        elif "ohayo" in message.content.lower().strip() or "oyasumi" in message.content.lower().strip():
+        elif (
+                "ohayo" in message.content.lower()
+                or "oyasumi" in message.content.lower()
+                or "geosta" in message.content.lower()
+                or "努力 未来 a geoffroyi star" in message.content.lower()
+        ):
             await message.add_reaction("\N{Black Heart}")
 
 
@@ -47,11 +54,11 @@ class Pengan(discord.Client):
 async def loop():
     now = datetime.datetime.now()
     if (now.hour, now.minute) == (22, 0):
-        await client.get_channel(bot_channel_id).send("ohayo")
+        await client.get_channel(main_channel_id).send("ohayo")
     elif (now.hour, now.minute) == (13, 0):
-        await client.get_channel(bot_channel_id).send("oyasumi")
+        await client.get_channel(main_channel_id).send("oyasumi")
     elif (now.hour, now.minute) == (15, 0):
-        await client.get_channel(bot_channel_id).send("geosta")
+        await client.get_channel(main_channel_id).send("geosta")
 
 
 client = Pengan()
