4e7f40d492a06686c694cca0ff5087393b04ac2f
EntityPENGIN <nopedoritos@gmail.com>
2023-05-24 18:14:47 +0900

    sheet

--- main.py
+++ main.py
@@ -9,24 +9,56 @@ import discord
 import discord.app_commands
 from discord.ext import tasks
 
+import radio
 from server import keep_alive
 
-discord_token = os.environ["DISCORD_TOKEN"]
-main_channel_id = int(os.environ["MAIN_CHANNEL_ID"])
-bot_channel_id = int(os.environ["BOT_CHANNEL_ID"])
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+MAIN_CHANNEL_ID = int(os.environ["MAIN_CHANNEL_ID"])
+BOT_CHANNEL_ID = int(os.environ["BOT_CHANNEL_ID"])
+SPREADSHEET_ID = os.environ["SPREADSHEET_ID"]
+
+SHEET_CREDS = {
+    "token": os.environ["SHEET_CREDS_TOKEN"],
+    "refresh_token": os.environ["SHEET_CREDS_REFRESH_TOKEN"],
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "client_id": os.environ["SHEET_CREDS_CLIENT_ID"],
+    "client_secret": os.environ["SHEET_CREDS_CLIENT_SECRET"],
+    "scopes": ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+    "expiry": "2023-05-24T08:48:49.831711Z"
+}
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 
+@tasks.loop(seconds=60)
+async def loop() -> None:
+    now = datetime.datetime.now()
+    if (now.hour, now.minute) == (22, 0):
+        await client.get_channel(MAIN_CHANNEL_ID).send("ohayo")
+    elif (now.hour, now.minute) == (13, 0):
+        await client.get_channel(MAIN_CHANNEL_ID).send("oyasumi")
+    elif (now.hour, now.minute) == (15, 0):
+        await client.get_channel(MAIN_CHANNEL_ID).send(
+            "geosta" if random.random() < 0.9 else "努力 未来 a geoffroyi star"
+        )
+    if now.minute % 2 == 0:
+        print(await client.check_radio_answers())
+
+
 class Pengan(discord.Client):
+    radio_answers_count: int
+
     def __init__(self):
         super().__init__(intents=intents)
 
-    async def on_ready(self):
+        self.radio_answers_count = len(await radio.get_answers(SPREADSHEET_ID, SHEET_CREDS))
+
+    async def on_ready(self) -> None:
         print(f"We have logged in as {self.user}")
         await self.change_presence(activity=discord.Game(name="!!help", type=1))
-        await client.get_channel(bot_channel_id).send(f"We have logged in as {self.user}")
+        await self.get_channel(BOT_CHANNEL_ID).send(f"We have logged in as {self.user}")
 
         # async for message in client.get_channel().history(limit=20):
         #     print(message.content)
@@ -34,7 +66,7 @@ class Pengan(discord.Client):
 
         loop.start()
 
-    async def on_message(self, message: discord.Message):
+    async def on_message(self, message: discord.Message) -> None:
         print(f"""
 On {message.channel}, {message.channel.guild} ({message.channel.id})
 {message.author}: {message.content}""")
@@ -44,26 +76,21 @@ On {message.channel}, {message.channel.guild} ({message.channel.id})
             await message.channel.send("ヘルプ: !!help")
 
         if "ohayo" in message.content.lower():
-            await message.add_reaction("\N{Black Heart}")
+            await message.add_reaction("\U0001f5a4")
         if "oyasumi" in message.content.lower():
             await message.add_reaction("<:emoji_2:1074290659135066163>")
         if "geosta" in message.content.lower() or "努力 未来 a geoffroyi star" in message.content.lower():
             await message.add_reaction("<:PENGIN_LV98:1097096256939114517>")
         if "充 電 し な き ゃ 　敵 の 命 で ね" in message.content.lower():
-            await message.add_reaction("\N{Black Heart}")
+            await message.add_reaction("\U0001f5a4")
 
-
-@tasks.loop(seconds=60)
-async def loop():
-    now = datetime.datetime.now()
-    if (now.hour, now.minute) == (22, 0):
-        await client.get_channel(main_channel_id).send("ohayo")
-    elif (now.hour, now.minute) == (13, 0):
-        await client.get_channel(main_channel_id).send("oyasumi")
-    elif (now.hour, now.minute) == (15, 0):
-        await client.get_channel(main_channel_id).send(
-            "geosta" if random.random() < 0.9 else "努力 未来 a geoffroyi star"
-        )
+    async def check_radio_answers(self) -> list[list[str]]:
+        answers = await radio.get_answers(SPREADSHEET_ID, SHEET_CREDS)
+        radio_answers_count = len(answers)
+        if 0 < (diff := radio_answers_count - self.radio_answers_count):
+            self.radio_answers_count = radio_answers_count
+            return answers[-diff:]
+        return []
 
 
 client = Pengan()
@@ -71,4 +98,4 @@ client = Pengan()
 
 keep_alive()
 
-client.run(discord_token)
+client.run(DISCORD_TOKEN)
---
+++ radio.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+async def get_answers(spreadsheet_id: str, sheet_creds_dict: dict[str, str]) -> list[list[str]]:
+    sheet_creds = Credentials.from_authorized_user_info(
+        sheet_creds_dict,
+        ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    )
+
+    try:
+        service = build("sheets", "v4", credentials=sheet_creds)
+
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=spreadsheet_id, range="A2:F").execute()
+        return result.get("values", [])
+
+    except HttpError:
+        return []
